5b74b130ceffb7edb060e890f094613b491771ef
amirmalamud <malamud.amir@s.afeka.ac.il>
2023-09-11 12:38:03 +0300

    Initial Commit

---
+++ SwiftUI_1/SwiftUI_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_1App.swift
+//  SwiftUI_1
+//
+//  Created by Amir Malamud on 11/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
